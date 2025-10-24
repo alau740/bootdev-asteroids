1e7a5f8065eafb48e8301399fd4cd17a8e885489
alau740 <77434904+alau740@users.noreply.github.com>
2025-10-25 00:32:00 +0100

    player rendering

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 # throughout this file
 import pygame
 from constants import *
+from player import Player
 
 clock = pygame.time.Clock()
 dt = 0
@@ -14,11 +15,13 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT: # window closed
                 return
         screen.fill(000000)
+        player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
         
