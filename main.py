8e0aff61c604ead95b6e08f61ab3302c2ffbfd3d
alau740 <77434904+alau740@users.noreply.github.com>
2025-10-28 23:26:35 +0000

    Add asteroid spawning

--- main.py
+++ main.py
@@ -4,13 +4,19 @@
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 clock = pygame.time.Clock()
 dt = 0
 
 updatable = pygame.sprite.Group()
 drawable = pygame.sprite.Group()
+asteroids = pygame.sprite.Group()
 Player.containers = (updatable, drawable)
+Asteroid.containers = (asteroids, updatable, drawable)
+AsteroidField.containers = (updatable)
+asteroidfield = AsteroidField()
 
 def main():
     pygame.init()
@@ -19,8 +25,8 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
+    dt = 0 # delta time
     while True:
-        dt = clock.tick(60) / 1000
         for event in pygame.event.get():
             if event.type == pygame.QUIT: # window closed
                 return
@@ -30,6 +36,8 @@ def main():
         for obj in drawable:
             obj.draw(screen)
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
 
         
     
